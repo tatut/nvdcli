1a5f22cdd455fd0efd5b3ab73a5b7524b3682269
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2026-04-25 14:39:53 +0300

    Implement fts query

---
+++ README.md
@@ -0,0 +1,4 @@
+# nvdcli
+
+nvdcli is a tool to download and query a local cache of NVD vulnerability data.
+It is blazing fast (because it's just sqlite under the hood).
--- nvdcli.c
+++ nvdcli.c
@@ -1,4 +1,6 @@
+#include <ctype.h>
 #include <math.h>
+#include <stdint.h>
 #include <stdio.h>
 #include "sqlite3.h"
 #include <curl/curl.h>
@@ -14,16 +16,27 @@ const char *default_db_file = "nvd.data";
 
 typedef enum cmd {
   USAGE,  // default command if no args given
-  SHOW,   // show info about CVE id
-  SEARCH, // fts search
+  SHOW,   // show human readable info about CVE id
+  GET,    // synonym for show, but sets output to raw json
+  SEARCH, // simple fts search
+
   BUILD,  // build the db from scratch
   UPDATE  // find any changes after the latest one, update those
 } cmd;
+
+typedef enum output {
+  ID,      // output only CVE ids, one per line
+  SHORT,   // short human readable output (default)
+  VERBOSE, // verbose human readable output
+  JSON     // full CVE JSON data (for tools)
+} output;
 
 typedef struct args {
   const char *db_file;
   const char *nvd_api_key;
   cmd command;
+  output fmt;
+  bool fmt_set; // is fmt set explicitly
   char *args[16]; // command args (first NULL means no more args)
 } args;
 
@@ -35,10 +48,11 @@ bool read_args(int argc, char **argv, args *a) {
   a->db_file = default_db_file;
   a->command = USAGE;
   a->nvd_api_key = getenv("NVD_API_KEY");
+  a->fmt = SHORT;
+  a->fmt_set = false;
   bool cmd = false;
   int cmd_args = 0;
   while(argc) {
-    printf(" argc: %d\n", argc);
     if(strcmp(argv[0], "--db-file")==0) {
       struct stat db_stat;
       int s;
@@ -69,6 +83,29 @@ bool read_args(int argc, char **argv, args *a) {
       a->nvd_api_key = argv[1];
       argc -= 2;
       argv += 2;
+    } else if (strcmp(argv[0], "--output") == 0) {
+      if (argc < 2) {
+        fprintf(stderr, "error: --output requires a parameter\n");
+        return false;
+      }
+      if (strcmp(argv[1], "id") == 0) {
+        a->fmt = ID;
+      } else if (strcmp(argv[1], "short") == 0) {
+        a->fmt = SHORT;
+      } else if (strcmp(argv[1], "verbose") == 0) {
+        a->fmt = VERBOSE;
+      } else if (strcmp(argv[1], "json") == 0) {
+        a->fmt = JSON;
+      } else {
+        fprintf(stderr,
+                "error: '%s' is not a valid format (use: id, short, verbose or "
+                "json)\n",
+                argv[1]);
+        return false;
+      }
+      a->fmt_set = true;
+      argc -= 2;
+      argv += 2;
     } else if (strncmp(argv[0], "--", 2) == 0) {
       fprintf(stderr, "error: unrecognized parameter %s\n", argv[0]);
       return false;
@@ -82,6 +119,8 @@ bool read_args(int argc, char **argv, args *a) {
           a->command = BUILD;
         } else if (strcmp(argv[0], "update") == 0) {
           a->command = UPDATE;
+        } else if (strcmp(argv[0], "get") == 0) {
+          a->command = GET;
         } else {
           fprintf(stderr, "Unrecognized command: %s\n", argv[0]);
           return false;
@@ -155,7 +194,6 @@ bool json_vuln_object(char **at, jobj *obj) {
   // printf("vuln object: %.*s\n", 100, obj->data);
   char *befo = *at;
   if (!json_skip(at)) {
-    printf("FAILED TO SKIP: %s\n", befo);
     return false;
   }
   obj->len = *at - obj->data;
@@ -216,6 +254,34 @@ bool json_insert_vuln(char **at, insert_ctx *ic) {
   return true;
 }
 
+#define SQ_CHECK(res)                                                          \
+  do {                                                                         \
+    if ((res) != SQLITE_OK) {                                                  \
+      fprintf(stderr, "[%s:%d] SQLite error: %s\n", __FILE__, __LINE__, sqlite3_errstr((res))); \
+      success = false;                                                         \
+      goto done;                                                               \
+    }                                                                          \
+  } while (false)
+
+
+bool sq_exec(sqlite3 *db, const char *sql) {
+  sqlite3_stmt *stmt = NULL;
+  int res;
+  bool success = true;
+
+  res = sqlite3_prepare(db, sql, -1, &stmt, NULL);
+  SQ_CHECK(res);
+  res = sqlite3_step(stmt);
+  if (res != SQLITE_DONE) {
+    fprintf(stderr, "Unexpected DDL statement return: %s\nDDL: %s\n",
+            sqlite3_errstr(res), sql);
+    success = false;
+  }
+ done:
+  if (stmt)
+    sqlite3_finalize(stmt);
+  return success;
+}
 /* build database, initialize the table, query all NVDs */
 bool build_db(args *a, sqlite3 *db) {
   sqlite3_stmt *stmt;
@@ -224,16 +290,15 @@ bool build_db(args *a, sqlite3 *db) {
   CURL *curl;
   bool success = true;
 
-  res = sqlite3_prepare(db,
-                        "CREATE TABLE IF NOT EXISTS cve ("
-                        "id TEXT PRIMARY KEY, "
-                        "data JSONB)",
-                        -1, &stmt, NULL);
-  if (res != SQLITE_OK) {
-    fprintf(stderr, "error: %s\n", sqlite3_errstr(res));
-    success = false;
-    goto done;
-  }
+  if (!sq_exec(db, "CREATE TABLE IF NOT EXISTS cve ("
+                   "id TEXT PRIMARY KEY, "
+                   "data JSONB)"))
+    return false;
+  if (!sq_exec(db,
+               "CREATE VIRTUAL TABLE cve_search USING fts5 (cve, description)"))
+    return false;
+
+  SQ_CHECK(res);
   res = sqlite3_step(stmt);
   if (res != SQLITE_DONE) {
     fprintf(stderr, "error: %s\n", sqlite3_errstr(res));
@@ -241,11 +306,7 @@ bool build_db(args *a, sqlite3 *db) {
     goto done;
   }
   res = sqlite3_finalize(stmt);
-  if (res != SQLITE_OK) {
-    fprintf(stderr, "error: %s\n", sqlite3_errstr(res));
-    success = false;
-    goto done;
-  }
+  SQ_CHECK(res);
 
   /* db ready, let's start pulling NVDs */
 https: // services.nvd.nist.gov/rest/json/cves/2.0/?resultsPerPage=20&startIndex=0
@@ -316,7 +377,11 @@ https: // services.nvd.nist.gov/rest/json/cves/2.0/?resultsPerPage=20&startIndex
     sleep(1); // is this enough?
     curl_slist_free_all(list);
     curl_easy_reset(curl);
-
+  }
+  printf("Fetch done, generating FTS5 search index.");
+  if (!sq_exec(db, "INSERT INTO cve_search (cve, description) SELECT id AS "
+               "cve, data->>'$.descriptions[0].value' FROM cve")) {
+    success = false;
   }
 
 done:
@@ -327,11 +392,252 @@ done:
   return success;
 }
 
+bool display(args *a, sqlite3 *db, char *id, char *cve_id,
+             char *cve_description);
+
 bool show(args *a, sqlite3 *db) {
-  printf("here it is \n");
+  int i = 0;
+  while (a->args[i] != NULL) {
+    if (!display(a, db, a->args[i], NULL, NULL))
+      return false;
+    i++;
+  }
   return true;
 }
+
+#define HL_START_TAG "<<HL"
+#define HL_END_TAG "HL>>"
+
+void print_hl(char *in, char *hl_start, char *hl_end, int max_len) {
+  char *at = in;
+  int len = strlen(in);
+  if (max_len < len)
+    len = max_len;
+  bool in_hl = false;
+  while (len > 0) {
+    if (*at == '<') {
+      // printf("at: %.*s, start? %s\n", 10, at,
+      //     strncmp(at, HL_START_TAG, 4)==0 ? "true" : "false");
+
+    }
+    if (strncmp(at, HL_START_TAG, 4) == 0) {
+      printf("%s", hl_start);
+      at += 4;
+      len -= 4;
+      in_hl = true;
+      continue;
+    } else if (strncmp(at, HL_END_TAG, 4) == 0) {
+      printf("%s", hl_end);
+      at += 4;
+      len -= 4;
+      in_hl = false;
+      continue;
+    }
+    if(isprint(*at)) {
+      printf("%c", *at);
+    } else {
+      printf("[%d]", *at);
+    }
+    at++;
+    len--;
+  }
+  if (in_hl)
+    printf("%s", hl_end);
+}
+
+#define HL_START "\e[1;92m"
+#define HL_END "\e[0m"
+#define TRUNC "\e[44m[...]\e[0m"
+
+const char *display_query[] = {
+    // just the id
+    [ID] = "SELECT id FROM cve WHERE id=?",
+    // publication date, severity and description
+    [SHORT] = "SELECT data->>'$.published',"
+              "       data->>'$.metrics.cvssMetricV2[0].baseSeverity',"
+              "       data->>'$.descriptions[0].value'"
+              "  FROM cve WHERE id=?",
+    // recursive tree walk of the json, so we can format all of it
+    [VERBOSE] = "SELECT key, value, type, t.id, parent FROM cve, "
+                "json_tree(data) t WHERE cve.id=?",
+
+    // get the data as JSON
+    [JSON] = "SELECT data FROM cve WHERE id=?"};
+
+void indent(int level) {
+  for (int i = 0; i < level; i++)
+    putchar(' ');
+}
+
+/* recursively display the json tree,
+ * returns true if the fetched row is still unhandled,
+ * this is for when returning from nested calls
+ */
+bool display_verbose(int level, int expected_parent, sqlite3_stmt *stmt,
+                     bool in_array) {
+  int res, id, parent;
+  char *key, *type, *id_str, *parent_str;
+
+ start:
+  key = (char*) sqlite3_column_text(stmt, 0);
+  type = (char*) sqlite3_column_text(stmt, 2);
+  id_str = (char*) sqlite3_column_text(stmt, 3);
+  parent_str = (char*) sqlite3_column_text(stmt, 4);
+
+  id = id_str ? atoi(id_str) : -1;
+   parent = parent_str ? atoi(parent_str) : -1;
+
+  if(expected_parent != parent) return true; // done with
+
+  if(key != NULL) {
+    indent(level);
+    printf("\e[0;36m%s\e[0m: ", key);
+  }
+
+  if (strcmp(type, "array") == 0) {
+    // output the key, then recurse to children
+    res = sqlite3_step(stmt);
+    if (res == SQLITE_DONE)
+      return false;
+    printf("\n");
+    if(display_verbose(level + 2, id, stmt, true)) goto start;
+  } else if (strcmp(type, "object") == 0) {
+    res = sqlite3_step(stmt);
+    if (res == SQLITE_DONE)
+      return false;
+    printf("\n");
+    bool unhandled = display_verbose(level + 2, id, stmt, false);
+    if(unhandled) goto start;
+  } else {
+    char *type_color = "\e[0;37m";
+    char *value = sqlite3_column_text(stmt, 1);
+    if (strcmp(type, "text") == 0) {
+      type_color = "\e[0;34m";
+    } else if (strcmp(type, "real") == 0) {
+      type_color = "\e[0;35m";
+    } else if (strcmp(type, "false") == 0) {
+      type_color = "\e[0;31m";
+      value = "false";
+    } else if (strcmp(type, "true") == 0) {
+      type_color = "\e[0;32m";
+      value = "true";
+    }
+    printf("%s%s\e[0m\n", type_color, value);
+    res = sqlite3_step(stmt);
+    if (res == SQLITE_DONE)
+      return false;
+    goto start;
+  }
+
+  return false; // this row was handled
+
+}
+/* Generic display, fetches data by id.
+ * If cve_id, cve_description are non-NULL (from search with possible highlight)
+ * those are used in place of the fetched ones.
+ */
+bool display(args *a, sqlite3 *db, char *id, char *cve_id, char *cve_description) {
+  static sqlite3_stmt *stmt = NULL; // reused on successive calls
+  int ret;
+
+  bool success = true;
+  if (stmt == NULL) {
+    printf("preparing: %s\n", display_query[a->fmt]);
+    ret =
+        sqlite3_prepare(db, display_query[a->fmt], -1, &stmt, NULL);
+    SQ_CHECK(ret);
+  }
+  ret = sqlite3_bind_text(stmt, 1, id, -1, NULL);
+  SQ_CHECK(ret);
+  ret = sqlite3_step(stmt);
+  if (ret != SQLITE_ROW) {
+    fprintf(stderr, "No such CVE: %s\n", id);
+    return false;
+  }
+  switch (a->fmt) {
+  case ID:
+    printf("%s\n", id);
+    break;
+  case SHORT: {
+    if (cve_id)
+      print_hl(cve_id, HL_START, HL_END, 256);
+    else
+      printf("%s", id);
+    printf("\n published: %s\n severity: %s\n", sqlite3_column_text(stmt, 0),
+           sqlite3_column_text(stmt, 1));
+    char *desc = cve_description;
+    if (!desc) {
+      desc = (char*)sqlite3_column_text(stmt, 2);
+    }
+    size_t len = strlen(desc);
+    char *hl = strstr(desc, HL_START_TAG);
+    if (hl - desc > 90) {
+      printf(TRUNC);
+      print_hl(hl - 10, HL_START, HL_END, 100);
+    } else {
+      print_hl(desc, HL_START, HL_END, 100);
+    }
+    if (len > 100)
+      printf(TRUNC);
+    printf("\n");
+    break;
+  }
+  case VERBOSE:
+    display_verbose(0, -1, stmt, false);
+    break;
+  case JSON:
+    printf("%s", sqlite3_column_text(stmt, 0));
+    break;
+  }
+
+done:
+  if (stmt) {
+    sqlite3_reset(stmt);
+    sqlite3_clear_bindings(stmt);
+  }
+  return success;
+}
+
+
+bool search(args *a, sqlite3 *db) {
+  bool success = true;
+  int res, i = 0;
+  sqlite3_stmt *stmt = NULL;
+  res = sqlite3_prepare(db,
+                        "SELECT highlight(cve_search, 0, '<<HL', 'HL>>'),"
+                        "       highlight(cve_search, 1, '<<HL','HL>>') "
+                        "  FROM cve_search"
+                        " WHERE (cve MATCH ?1)"
+                        "    OR (description MATCH ?1)"
+                        " ORDER BY rank", -1, &stmt, NULL);
+  SQ_CHECK(res);
+  while (a->args[i] != NULL) {
+    res = sqlite3_bind_text(stmt, 1, a->args[i], -1, NULL);
+    SQ_CHECK(res);
+    res = sqlite3_step(stmt);
+    while (res == SQLITE_ROW) {
+      char *id = (char *)sqlite3_column_text(stmt, 0);
+      char *desc = (char *)sqlite3_column_text(stmt, 1);
+      display(a, db, a->args[i], id, desc);
+      res = sqlite3_step(stmt);
+    }
+    if (res != SQLITE_DONE) {
+      fprintf(stderr, "Unexpected sqlite status: %s\n", sqlite3_errstr(res));
+      success = false;
+      goto done;
+    }
+    sqlite3_reset(stmt);
+    sqlite3_clear_bindings(stmt);
+    i++;
+  }
+
+done:
+  if(stmt) sqlite3_finalize(stmt);
+  return success;
+}
+
 int main(int argc, char **argv) {
+  int ret = 0;
   args args = {0};
   if (!read_args(argc - 1, argv + 1, &args))
     return 1;
@@ -339,7 +645,6 @@ int main(int argc, char **argv) {
   if (!open_db(&args, &db))
     return 1;
 
-  printf("command is %d\n", args.command);
   switch(args.command) {
   case USAGE:
     printf("Usage: nvdcli [options] command [...command args...]\n"
@@ -359,16 +664,24 @@ int main(int argc, char **argv) {
     break;
   case BUILD:
     if (!build_db(&args, db))
-      return 1;
+      ret = 1;
     break;
+  case GET:
+    if(!args.fmt_set)
+      args.fmt = JSON;
+    //fallthrough
   case SHOW:
     if (!show(&args, db))
-      return 1;
+      ret = 1;
+    break;
+  case SEARCH:
+    if (!search(&args, db))
+      ret = 1;
     break;
   default:
     fprintf(stderr, "Not yet implemented: %d\n", args.command);
   }
 
   sqlite3_close(db);
-  return 0;
+  return ret;
 }
